01423853afdf48f16086637acb0fd7d647d3ab83
AleksanderWojsz <pieczalek@gmail.com>
2024-09-08 12:16:57 +0200

    Initial files + name and photo on the main page

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+    <div class="flex flex-row justify-evenly items-center">
+        <div>
+            <p class="text-3xl" >Hi, I am</p>
+            <p class="text-4xl font-bold"><span>Michal Lewandowski</span></p>
+        </div>
+        <div class="w-1/2 max-w-[600px] h-auto">
+            <img src="C:\Users\wojsz\Downloads\myphoto_edge1.png">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
