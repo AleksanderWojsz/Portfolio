0dade808b4ad03254e366afa34b6fbf4ccf72af9
AleksanderWojsz <pieczalek@gmail.com>
2024-09-15 21:41:52 +0200

    changed fadein animation

--- src/components/ClassesTaken.vue
+++ src/components/ClassesTaken.vue
@@ -3,19 +3,22 @@
 </script>
 
 <template>
-    <div class="fade-in-right">
-        <p class="w-fit m-auto text-xl">Other classes taken</p>
-        <div class="paper-thin flex flex-col space-y-2 mt-4 pt-4">
+    <div style="overflow-x: hidden">
+        <div class="fade-in-right">
+            <p class="w-fit m-auto text-xl">Other classes taken</p>
+            <div class="paper-thin flex flex-col space-y-2 mt-4 pt-4">
 
-            <list_el>Mathematical analysis</list_el>
-            <list_el>Linear Algebra</list_el>
-            <list_el>Set theory</list_el>
-            <list_el>Discrete Mathematics</list_el>
-            <list_el>Probability theory</list_el>
-            <list_el>Languages and Automata Theory</list_el>
+                <list_el>Mathematical analysis</list_el>
+                <list_el>Linear Algebra</list_el>
+                <list_el>Set theory</list_el>
+                <list_el>Discrete Mathematics</list_el>
+                <list_el>Probability theory</list_el>
+                <list_el>Languages and Automata Theory</list_el>
 
+            </div>
         </div>
     </div>
+
 
 </template>
 
