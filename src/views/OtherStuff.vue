ec397bce013a8ad8131c3ec9f8a959e33f5755ba
AleksanderWojsz <pieczalek@gmail.com>
2024-09-16 09:53:40 +0200

    fixed typo

--- src/views/OtherStuff.vue
+++ src/views/OtherStuff.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="flex flex-col items-center space-y-6 mt-10">
         <div class="fade-in-left paper-no-animation flex flex-col items-center">
-            <p class="my-3 text-lg">An older photo I often have as profile picture</p>
+            <p class="my-3 text-lg">An older photo I often have as a profile picture</p>
             <img class="h-[400px] w-auto" src="/myphoto.jpg" alt="photo of me">
         </div>
     </div>
